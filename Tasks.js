994c765c225eb21d07a39c9b251b78388f2bf44c
Fabio Morais <fbiosmor@gmail.com>
2026-01-11 19:51:31 -0300

    Almost complete version

--- Tasks.js
+++ Tasks.js
@@ -1,3 +1,41 @@
+// function addTask(task){
+//     if(task === ""){
+//         alert("button clicked");
+//     }
+// }
+
+let myAllTasks = [];
+
 function addTask(){
-    alert("button clicked");
+
+    let input = document.getElementById("addTask");
+    let digitedValue = input.value;
+
+    if (digitedValue === "") {
+        alert("Please enter a task.");
+        return;
+    }
+
+    myAllTasks.push(digitedValue);
+    console.log(myAllTasks);
+
+    let newLine = document.createElement("li");
+    newLine.innerText = digitedValue;
+
+    newLine.onclick = function() {
+        this.classList.toggle("completed");
+    }
+
+    let buttonDelete = document.createElement("button");
+    buttonDelete.innerText = "Delete";
+    buttonDelete.classList.add("delete-btn");
+    buttonDelete.onclick = function(event) {
+        event.stopPropagation();
+        newLine.remove();
+    }
+    newLine.appendChild(buttonDelete);
+
+    let taskList = document.getElementById("taskList");
+    taskList.appendChild(newLine);
+    input.value = "";
 }
