c551ce872fb3e9f855606d365409525684791931
Fabio Morais <fbiosmor@gmail.com>
2026-01-12 19:06:09 -0300

    Final version

--- Tasks.js
+++ Tasks.js
@@ -1,13 +1,6 @@
-// function addTask(task){
-//     if(task === ""){
-//         alert("button clicked");
-//     }
-// }
-
 let myAllTasks = [];
 
-function addTask(){
-
+function addTask() {
     let input = document.getElementById("addTask");
     let digitedValue = input.value;
 
@@ -16,26 +9,60 @@ function addTask(){
         return;
     }
 
-    myAllTasks.push(digitedValue);
-    console.log(myAllTasks);
+    let itemObject = {
+        text: digitedValue,
+        concluded: false
+    };
 
+    myAllTasks.push(itemObject);
+    localStorage.setItem("myTasks", JSON.stringify(myAllTasks));
+
+    assembleTaskList(itemObject);
+    input.value = "";
+}
+
+const savedTasks = localStorage.getItem("myTasks");
+
+if (savedTasks) {
+    myAllTasks = JSON.parse(savedTasks);
+
+    myAllTasks.forEach(function(taskObject) {
+        assembleTaskList(taskObject);
+    });
+};
+   
+function assembleTaskList(item) {
     let newLine = document.createElement("li");
-    newLine.innerText = digitedValue;
+    newLine.innerText = item.text;
 
+    if (item.concluded === true) {
+        newLine.classList.add("completed");
+    }
     newLine.onclick = function() {
         this.classList.toggle("completed");
-    }
+        
+        item.concluded = !item.concluded;
+        localStorage.setItem("myTasks", JSON.stringify(myAllTasks));
+}
 
     let buttonDelete = document.createElement("button");
     buttonDelete.innerText = "Delete";
     buttonDelete.classList.add("delete-btn");
+
     buttonDelete.onclick = function(event) {
         event.stopPropagation();
+
         newLine.remove();
+        let position = myAllTasks.indexOf(item);
+
+        if (position !== -1) {
+            myAllTasks.splice(position, 1);
+        }
+        localStorage.setItem("myTasks", JSON.stringify(myAllTasks));
     }
     newLine.appendChild(buttonDelete);
 
-    let taskList = document.getElementById("taskList");
-    taskList.appendChild(newLine);
-    input.value = "";
-}
+    document.getElementById("taskList").appendChild(newLine);
+}      
+
+
